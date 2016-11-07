c3991efc412d9e1c9ad5ceda2b95c79116ad9462
System Administrator <root@MacBook-Pro.local>
2016-11-07 11:14:40 -0800

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,82 @@
+# Copyright (c) 2016. TIBCO Software Inc.
+# This file is subject to the license terms contained
+# in the license file that is distributed with this file.
+# version: 6.3.0-v1.0.2
+FROM tomcat:8.0-jre8
+
+# Copy jasperreports-server-<ver> zip file from resources dir.
+# Build will fail if file not present.
+COPY resources/jasperreports-server*zip /tmp/jasperserver.zip
+
+RUN apt-get update && apt-get install -y postgresql-client unzip xmlstarlet && \
+    rm -rf /var/lib/apt/lists/* && \
+    unzip /tmp/jasperserver.zip -d /usr/src/ && \
+    mv /usr/src/jasperreports-server-* /usr/src/jasperreports-server && \
+    mkdir -p /usr/local/share/jasperreports-pro/license && \
+    rm -rf /tmp/*
+
+# Extract phantomjs, move to /usr/local/share/phantomjs, link to /usr/local/bin.
+# Comment out if phantomjs not required.
+RUN wget \
+    "https://bitbucket.org/ariya/phantomjs/downloads/\
+phantomjs-2.1.1-linux-x86_64.tar.bz2" \
+    -O /tmp/phantomjs.tar.bz2 --no-verbose && \
+    tar -xjf /tmp/phantomjs.tar.bz2 -C /tmp && \
+    rm -f /tmp/phantomjs.tar.bz2 && \
+    mv /tmp/phantomjs*linux-x86_64 /usr/local/share/phantomjs && \
+    ln -sf /usr/local/share/phantomjs/bin/phantomjs /usr/local/bin && \
+    rm -rf /tmp/*
+# In case you wish to download from a different location you can manually
+# download the archive and copy from resources/ at build time. Note that you
+# also # need to comment out the preceding RUN command
+#COPY resources/phantomjs*bz2 /tmp/phantomjs.tar.bz2
+#RUN tar -xjf /tmp/phantomjs.tar.bz2 -C /tmp && \
+#    rm -f /tmp/phantomjs.tar.bz2 && \
+#    mv /tmp/phantomjs*linux-x86_64 /usr/local/share/phantomjs && \
+#    ln -sf /usr/local/share/phantomjs/bin/phantomjs /usr/local/bin && \
+#    rm -rf /tmp/*
+
+# Set default environment options.
+ENV CATALINA_OPTS="${JAVA_OPTIONS:--Xmx2g -XX:+UseParNewGC \
+    -XX:+UseConcMarkSweepGC} \
+    -Djs.license.directory=${JRS_LICENSE:-/usr/local/share/jasperreports-pro/license}"
+
+# Configure tomcat for SSL (optional). Uncomment ENV and RUN to enable generation of
+# self-signed certificate and to set up JasperReports Server to use HTTPS only.
+#
+#ENV DN_HOSTNAME=${DN_HOSTNAME:-localhost.localdomain} \
+#    KS_PASSWORD=${KS_PASSWORD:-changeit} \
+#    JRS_HTTPS_ONLY=${JRS_HTTPS_ONLY:-true} \
+#    HTTPS_PORT=${HTTPS_PORT:-8443}
+#
+#RUN keytool -genkey -alias self_signed -dname "CN=${DN_HOSTNAME}" \
+#        -keyalg RSA -storepass "${KS_PASSWORD}" \
+#        -keypass "${KS_PASSWORD}" \
+#        -keystore /root/.keystore && \
+#    xmlstarlet ed --inplace --subnode "/Server/Service" --type elem \
+#        -n Connector -v "" --var connector-ssl '$prev' \
+#    --insert '$connector-ssl' --type attr -n port -v "${HTTPS_PORT:-8443}" \
+#    --insert '$connector-ssl' --type attr -n protocol -v \
+#        "org.apache.coyote.http11.Http11NioProtocol" \
+#    --insert '$connector-ssl' --type attr -n maxThreads -v "150" \
+#    --insert '$connector-ssl' --type attr -n SSLEnabled -v "true" \
+#    --insert '$connector-ssl' --type attr -n scheme -v "https" \
+#    --insert '$connector-ssl' --type attr -n secure -v "true" \
+#    --insert '$connector-ssl' --type attr -n clientAuth -v "false" \
+#    --insert '$connector-ssl' --type attr -n sslProtocol -v "TLS" \
+#    --insert '$connector-ssl' --type attr -n keystorePass \
+#        -v "${KS_PASSWORD}"\
+#    --insert '$connector-ssl' --type attr -n keystoreFile \
+#        -v "/root/.keystore" \
+#    ${CATALINA_HOME}/conf/server.xml
+
+# Expose ports. Note that you must do one of the following:
+# map them to local ports at container runtime via "-p 8080:8080 -p 8443:8443"
+# or use dynamic ports.
+EXPOSE ${HTTP_PORT:-8080} ${HTTPS_PORT:-8443}
+
+COPY scripts/entrypoint.sh /
+ENTRYPOINT ["/entrypoint.sh"]
+
+# Default action executed by entrypoint script.
+CMD ["run"]
---
+++ scripts/entrypoint.sh
@@ -0,0 +1,230 @@
+#!/bin/bash
+
+# Copyright (c) 2016. TIBCO Software Inc.
+# This file is subject to the license terms contained
+# in the license file that is distributed with this file.
+# version: 6.3.0-v1.0.2
+
+# This script sets up and runs JasperReports Server on container start.
+# Default "run" command, set in Dockerfile, executes run_jasperserver.
+# If webapps/jasperserver-pro does not exist, run_jasperserver 
+# redeploys webapp. If "jasperserver" database does not exist,
+# run_jasperserver redeploys minimal database.
+# Additional "init" only calls init_database, which will try to recreate 
+# database and fail if DB exists.
+
+# Sets script to fail if any command fails.
+set -e
+
+setup_jasperserver() {
+  # If environment is not set, uses default values for postgres
+  DB_USER=${DB_USER:-postgres}
+  DB_PASSWORD=${DB_PASSWORD:-postgres}
+  DB_HOST=${DB_HOST:-postgres}
+  DB_PORT=${DB_PORT:-5432}
+  DB_NAME=${DB_NAME:-jasperserver}
+
+  # Simple default_master.properties. Modify according to
+  # JasperReports Server documentation.
+  cat >/usr/src/jasperreports-server/buildomatic/default_master.properties\
+<<-_EOL_
+appServerType=tomcat8
+appServerDir=$CATALINA_HOME
+dbType=postgresql
+dbHost=$DB_HOST
+dbUsername=$DB_USER
+dbPassword=$DB_PASSWORD
+dbPort=$DB_PORT
+js.dbName=$DB_NAME
+_EOL_
+
+  # Execute js-ant targets for installing/configuring
+  # JasperReports Server. Note that js-ant should be
+  # executed in buildomatic directory.
+  cd /usr/src/jasperreports-server/buildomatic/
+  for i in $@; do
+    # Default deploy-webapp-pro attempts to remove
+    # $CATALINA_HOME/webapps/jasperserver-pro path.
+    # This behaviour does not work if mounted volumes are used.
+    # Uses unzip to populate webapp directory and non-destructive
+    # targets for configuration
+    if [ $i == "deploy-webapp-pro" ]; then
+      mkdir -p $CATALINA_HOME/webapps/jasperserver-pro ;
+      unzip -o -q ../jasperserver-pro.war \
+        -d $CATALINA_HOME/webapps/jasperserver-pro
+      ./js-ant \
+        init-source-paths \
+        set-pro-webapp-name \
+        deploy-webapp-datasource-configs \
+        deploy-jdbc-jar \
+        -DwarTargetDir=$CATALINA_HOME/webapps/jasperserver-pro
+    else
+      # warTargetDir and webaAppName are set as
+      # workaround for database configuration regeneration.
+      ./js-ant $i \
+        -DwarTargetDir=$CATALINA_HOME/webapps/jasperserver-pro \
+        -DwebAppName=jasperserver-pro
+    fi
+  done
+}
+
+run_jasperserver() {
+  # If jasperserver-pro webapp is not present or if only WEB-INF/logs present
+  # in tomcat webapps directory do deploy-webapp-pro.
+  # Starts upon webapp deployment as database may still be initializing.
+  # This speeds up overall startup because deploy-webapp-pro does
+  # not depend on database.
+  if [[ -d "$CATALINA_HOME/webapps/jasperserver-pro" ]]; then
+    if [[ `ls -1 $CATALINA_HOME/webapps/jasperserver-pro| wc -l` -le 1 \
+      || `ls -1 -v $CATALINA_HOME/webapps/jasperserver-pro| head -n 1` \
+      =~ "WEB-INF.*" ]]; then
+        setup_jasperserver deploy-webapp-pro
+    fi
+  else
+    setup_jasperserver deploy-webapp-pro
+  fi
+
+    
+  # Wait for PostgreSQL.
+  retry_postgresql
+
+  # Force regeneration of database configuration if variable is set.
+  # This supports changes to DB configuration for already created
+  # container.
+  if [[ ${JRS_DBCONFIG_REGEN} ]]; then
+    setup_jasperserver deploy-webapp-datasource-configs deploy-jdbc-jar
+  fi
+ 
+  # Set up jasperserver database if it is not present.
+  if [[ `test_postgresql -l | grep -i ${DB_NAME:-jasperserver} | wc -l` < 1 \
+    ]]; then
+    setup_jasperserver set-pro-webapp-name \
+      create-js-db \
+      init-js-db-pro \
+      import-minimal-pro
+  fi
+
+  # Run deploy-jdbc-jar in the case that the tomcat container has been updated.
+  setup_jasperserver deploy-jdbc-jar
+
+  config_license
+
+  # Set up phantomjs.
+  config_phantomjs
+
+  # If JRS_HTTPS_ONLY is set, set JasperReports Server to
+  # run only in HTTPS.
+  config_ssl
+
+  # Apply customization zip if present.
+  config_customization
+
+  # Start tomcat.
+  catalina.sh run
+}
+
+init_database() {
+  # Wait for PostgreSQL.
+  retry_postgresql
+  # Run-only db creation targets.
+  setup_jasperserver create-js-db init-js-db-pro import-minimal-pro
+}
+
+# Initial license handling.
+config_license() {
+  # If license file does not exist, copy evaluation license.
+  # Non-default location (~/ or /root) used to allow
+  # for storing license in a volume. To update license,
+  # replace license file and restart container
+  JRS_LICENSE=${JRS_LICENSE:-/usr/local/share/jasperreports-pro/license}
+  if [[ ! -f \
+    "${JRS_LICENSE}/jasperserver.license"\
+    ]]; then
+    cp /usr/src/jasperreports-server/jasperserver.license \
+      /usr/local/share/jasperreports-pro/license
+  fi
+}
+
+test_postgresql() {
+  export PGPASSWORD=${DB_PASSWORD:-postgres}
+  psql -h ${DB_HOST:-postgres} -p ${DB_PORT:-5432} -U ${DB_USER:-postgres} $@
+}
+
+retry_postgresql() {
+  # Retry 5 times to check PostgreSQL is accessible.
+  for retry in {1..5}; do
+    test_postgresql && echo "PostgreSQL accepting connections" && break || \
+      echo "Waiting for PostgreSQL..." && sleep 10;
+  done
+
+  # Fail if PostgreSQL is not accessible
+  test_postgresql || \
+    echo "Error: PostgreSQL on ${DB_HOST:-postgres} not accessible!"
+}
+
+config_phantomjs() {
+  # if phantomjs binary is present, update JaseperReports Server config.
+  if [[ -x "/usr/local/bin/phantomjs" ]]; then
+    PATH_PHANTOM='\/usr\/local\/bin\/phantomjs'
+    PATTERN1='com.jaspersoft.jasperreports'
+    PATTERN2='phantomjs.executable.path'
+    cd $CATALINA_HOME/webapps/jasperserver-pro/WEB-INF
+    sed -i -r "s/(.*)($PATTERN1.highcharts.$PATTERN2=)(.*)/\2$PATH_PHANTOM/" \
+      classes/jasperreports.properties
+    sed -i -r "s/(.*)($PATTERN1.fusion.$PATTERN2=)(.*)/\2$PATH_PHANTOM/" \
+      classes/jasperreports.properties
+    sed -i -r "s/(.*)(phantomjs.binary=)(.*)/\2$PATH_PHANTOM/" \
+      js.config.properties
+  elif [[ "$(ls -A /usr/local/share/phantomjs)" ]]; then
+    echo "Warning: /usr/local/bin/phantomjs is not executable, \
+but /usr/local/share/phantomjs exists. PhantomJS \
+is not correctly configured."
+  fi
+}
+
+# Apply other runtime configurations.
+config_ssl() {
+  # If $JRS_HTTPS_ONLY is set in environment to "true", disable HTTP support
+  # in JasperReports Server.
+  if [[ $JRS_HTTPS_ONLY ]]; then
+    cd $CATALINA_HOME/webapps/jasperserver-pro/WEB-INF
+    xmlstarlet ed --inplace \
+      -N x="http://java.sun.com/xml/ns/j2ee" -u \
+      "//x:security-constraint/x:user-data-constraint/x:transport-guarantee"\
+      -v "CONFIDENTIAL" web.xml
+    sed -i "s/=http:\/\//=https:\/\//g" js.quartz.properties
+    sed -i "s/${HTTP_PORT:-8080}/${HTTPS_PORT:-8443}/g" js.quartz.properties
+  fi
+
+}
+
+config_customization() {
+  # Unpack zips (if they exist) from the path
+  # /usr/local/share/jasperreports-pro/customization
+  # to the JasperReports Server web application path
+  # $CATALINA_HOME/webapps/jasperserver-pro/
+  # File sorted with natural sort.
+  JRS_CUSTOMIZATION=\
+${JRS_CUSTOMIZATION:-/usr/local/share/jasperreports-pro/customization}
+  JRS_CUSTOMIZATION_FILES=`find $JRS_CUSTOMIZATION -iname "*zip" \
+    -exec readlink -f {} \; | sort -V`
+  for customization in $JRS_CUSTOMIZATION_FILES; do
+    if [[ -f "$customization" ]]; then
+      unzip -o -q "$customization" \
+        -d $CATALINA_HOME/webapps/jasperserver-pro/
+    fi
+  done
+}
+
+case "$1" in
+  run)
+    shift 1
+    run_jasperserver "$@"
+    ;;
+  init)
+    init_database
+    ;;
+  *)
+    exec "$@"
+esac
+
