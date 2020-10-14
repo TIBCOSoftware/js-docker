4dc755fa87ec94d3f2f50f06bc03ed52cfbe9c55
RangaReddyV <36688572+RangaReddyV@users.noreply.github.com>
2020-10-14 11:08:42 +0530

    ENGINFRA-8743 Fixing the k8s issues

--- kubernetes/scripts/installPackagesForJasperserver-pro-cmdline-k8s.sh
+++ kubernetes/scripts/installPackagesForJasperserver-pro-cmdline-k8s.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (c) 2019. TIBCO Software Inc.
+# Copyright (c) 2020. TIBCO Software Inc.
 # This file is subject to the license terms contained
 # in the license file that is distributed with this file.
 
