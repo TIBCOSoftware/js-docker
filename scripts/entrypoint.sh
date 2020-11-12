cdc542a4122d5b61d48c4985d52ef036e1e9cb92
RangaReddyV <36688572+RangaReddyV@users.noreply.github.com>
2020-11-12 12:41:40 +0530

    /remove self signed certificate @ENGINFRA-8791

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -102,34 +102,8 @@ config_ports_and_ssl() {
     else
       echo "No .keystore files. Did not update SSL"
     fi
-
   # end if $SSL_CERT_PATH exists.
-  else
-   #  keytool -genkey -alias self_signed -dname "CN=${DN_HOSTNAME}" \
-    #    -storetype PKCS12 \
-    #   -storepass "${KS_PASSWORD}" \
-    #    -keypass "${KS_PASSWORD}" \
-    #    -keystore $CATALINA_HOME/conf/.keystore.p12 && \
-    #keytool -list -keystore $CATALINA_HOME/conf/.keystore.p12 -storepass "${KS_PASSWORD}" -storetype PKCS12 && \
-    #xmlstarlet ed --inplace --subnode "/Server/Service" --type elem \
-    #    -n Connector -v "" --var connector-ssl '$prev' \
-    #--insert '$connector-ssl' --type attr -n port -v "${HTTPS_PORT}" \
-    #--insert '$connector-ssl' --type attr -n protocol -v \
-    #    "org.apache.coyote.http11.Http11NioProtocol" \
-    #--insert '$connector-ssl' --type attr -n maxThreads -v "150" \
-    #--insert '$connector-ssl' --type attr -n SSLEnabled -v "true" \
-    #--insert '$connector-ssl' --type attr -n scheme -v "https" \
-    #--insert '$connector-ssl' --type attr -n secure -v "true" \
-    #--insert '$connector-ssl' --type attr -n clientAuth -v "false" \
-    #--insert '$connector-ssl' --type attr -n sslProtocol -v "TLS" \
-    #--insert '$connector-ssl' --type attr -n keystorePass \
-    #    -v "${KS_PASSWORD}"\
-    #--insert '$connector-ssl' --type attr -n keystoreFile \
-    #    -v "$CATALINA_HOME/conf/.keystore.p12" \
-    #${CATALINA_HOME}/conf/server.xml
-
   fi
-
 }
 
 apply_customizations() {
